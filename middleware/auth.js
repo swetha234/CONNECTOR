aa847ec8a66541cea053ba35390861e26ad6f451
swetha234 <hgutt001@odu.edu>
2019-09-10 15:12:38 -0400

    second coomit

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  //Get token from header that is displayed
+  const token = req.header('x-auth-token');
+
+  //check if no token
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: 'No token found, authorization denied' });
+  }
+
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, cofig.get('jwtToken'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
